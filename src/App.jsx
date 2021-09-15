9847f51b4295712c6e646d9b996ef1baaf14533c
restridsman <rstridsman@gmail.com>
2021-09-15 13:51:13 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import './App.css'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
  
 
   return (
     <div className="App">
+        <Navbar />
         <Header />
     </div>
   )
